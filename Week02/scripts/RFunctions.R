e0f4595985b1b2f7d155680882bbf59ada4c25aa
Douglas Nychka <nychka@ucar.edu>
2017-01-23 12:57:40 -0700

    Week02

--- Week02/scripts/RFunctions.R
+++ Week02/scripts/RFunctions.R
@@ -2,15 +2,17 @@
 # functions
 #
   load("BT.rda")
-# or 
-  library( dataWorkshop)
-  data(BoulderJuneTemperature)
+
 # finding the inter quartile range:
   BT<-BoulderJuneTemperature$Temp
 # 75% quantile  
   BT75<-  quantile( BT,.75)
   
-#Q Find the interquartile range   75% - 25% quantiles  
+#Q Find the interquartile range   75% - 25% quantiles
+
+# check this against the built in function
+
+ IQR( BT)
 
 # functions can have many default and optional arguments
   
@@ -25,7 +27,31 @@
  
  #Q turn off the label of 75% on the value
  
- # building your own function 
+# building your own function
+# here is a function that adds the squares of two numbers
+# it has three parts, the calling arguments, the body where you do the
+# work and then returning any results
+myFun<- function( a,b){
+    result<- a^2 + b^2
+    return(result)
+}
+
+#you only copy and paste this into your session once
+
+  test1<- myFun( 2,3)
+  test2<- myFun(1:5, 11:15)
+# note that the a, b and result are only used inside the function and
+# do not appear in your workspace
+# also since the body is normal R code this works for vectors automatically
+  x<-1:5
+  y<- 11:15
+  myFun( x, y)
+
+
+
+
+
+
  
  myIQR <- function(y){
             IQR<- quantile( y, .75, names=FALSE) - 
@@ -36,7 +62,23 @@
   myIQR(BT)
   
 #Q modify this function to work with NAs
-  
+
+myIQR <- function(y, na.rm=FALSE){
+            IQR<- quantile( y, .75,  names=FALSE,  na.rm=na.rm) - 
+                  quantile( y, .25,  names=FALSE, na.rm=na.rm) 
+            return( IQR)
+ }
+
+
+myIQR <- function(y, na.rm=FALSE){
+# an example of adding a warning     
+    if( na.rm){
+        warning("NAs removed from the data")
+    }
+            IQR<- quantile( y, .75, names=FALSE, na.rm=na.rm) - 
+                  quantile( y, .25,  names=FALSE, na.rm=na.rm) 
+            return( IQR)
+ } 
 
   
   
--- Week02/scripts/boxplotDiagram.R
+++ Week02/scripts/boxplotDiagram.R
@@ -1,11 +1,14 @@
-
+# draws boxplot example to use in Lecture2.2 notes
 library( dataWorkshop)
 data( AudiA4)
+# or 
 fields.style()
+# mileage from audi data set used as example 
 y<- AudiA4$mileage/1000
-par( mar=c(3,3,1,1))
-plot( c(.5,2.5), c(0,200),xlab="", ylab="", type="n",
-      xaxt="n")
+par( mar=c(3,5,1,1))
+plot( c(.5,2.5), c(0,200),xlab="", type="n",
+      xaxt="n",ylab="mileage (1000)")
+
 statsBP<- boxplot( y, col="green4", add=TRUE)
 par(xpd=TRUE)
 text( 1.2,median(y)," <- Median (notch)", adj=0)
@@ -18,8 +21,14 @@ W2<- max( y[ y < ( Q75 +  1.5* IQR ) ])
 W1<- min( y[ y > ( Q25 -  1.5* IQR )] )
 text( 1.2,W2," <- whisker", adj=0)
 text( 1.2,W1," <- whisker", adj=0)
-outlier<- y[  y > W2 | y < W1]
-text(1.3,outlier, " outlier" )
-#points(rep( .5, length( y)), y, pch=15, cex=.4,col="grey20")
-dev.print( png, filename="boxplotFigure.png", height=480, width=480)
+# NOTE: | used in logical means "or" 
+outliers<- y[  (y > W2) | (y < W1) ]
+text(1.3,outliers, " outlier" )
+# add the actual data to show how difficult it is to see this information
+# but thi
+points(rep( .5, length( y)), y, pch=16, cex=.6,col="black")
+# dump the plot to a file in jpeg format
+
+dev.print( jpeg, filename="boxplotFigure.jpeg", height=600, width=600,
+           bg = "white")
 
